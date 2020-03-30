f78dd6eb9817f20fa22e27036e6a3744d53be743
HasSan-68 <48790045+HasSan-68@users.noreply.github.com>
2020-03-30 16:18:24 +0200

    changes
    changes

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -5,8 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Product;
-use App\Form\ProductType;
 use App\Repository\ProductRepository;
 
 /**
@@ -32,17 +30,12 @@ class CartController extends AbstractController
 
         // fetch the information using query and ids in the cart
         if ($cart != '') {
-
-
             if (isset($cart)) {
                 $product = $productRepository->findAll();
             } else {
                 return $this->render('Cart/index.html.twig', array(
                     'empty' => true,
                 ));
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
-        ]);
             }
 
 
@@ -56,5 +49,74 @@ class CartController extends AbstractController
             ));
         }
     }
+    /**
+     * @Route("/add/{id}", name="cart_add")
+     */
+    public function addAction($id,ProductRepository $productRepository)
+    {
+        // fetch the cart
+        //$em = $this->getDoctrine()->getManager();
+        $product = $productRepository->find($id);
+        //print_r($product->getId()); die;
+        $session = $this->get('request_stack')->getCurrentRequest()->getSession();
+        $cart = $session->get('cart', array());
 
+        // check if the $id already exists in it.
+        if ($product == NULL) {
+            $this->get('session')->setFlash('notice', 'This product is not     available in Stores');
+            return $this->redirect($this->generateUrl('cart'));
+        } else {
+            if (isset($cart[$id])) {
+
+                ++$cart[$id];
+                /*  $qtyAvailable = $product->getQuantity();
+                  if ($qtyAvailable >= $cart[$id]['quantity'] + 1) {
+                      $cart[$id]['quantity'] = $cart[$id]['quantity'] + 1;
+                  } else {
+                      $this->get('session')->setFlash('notice', 'Quantity     exceeds the available stock');
+                      return $this->redirect($this->generateUrl('cart'));
+                  } */
+            } else {
+                // if it doesnt make it 1
+                $cart[$id] = 1;
+                //$cart[$id]['quantity'] = 1;
+            }
+
+            $session->set('cart', $cart);
+            //echo('<pre>');
+            //print_r($cart); echo ('</pre>');die();
+            return $this->redirect($this->generateUrl('cart_view'));
+
+        }
+
+    }
+    /**
+     * @Route("/remove/{id}", name="cart_remove")
+     */
+    public function removeAction($id)
+    {
+        // check the cart
+        $session = $this->get('request_stack')->getCurrentRequest()->getSession();
+        $cart = $session->get('cart', array());
+
+        // if it doesn't exist redirect to cart index page. end
+        if(!$cart[$id]) { $this->redirect( $this->generateUrl('cart_view') ); }
+
+        // check if the $id already exists in it.
+        if( isset($cart[$id]) ) {
+            $cart[$id] = $cart[$id] - 1;
+            if ($cart[$id] < 1) {
+                unset($cart[$id]);
+            }
+        } else {
+            return $this->redirect( $this->generateUrl('cart_view') );
+        }
+
+        $session->set('cart', $cart);
+
+        //echo('<pre>');
+        //print_r($cart); echo ('</pre>');die();
+
+        return $this->redirect( $this->generateUrl('cart') );
+    }
 }
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -83,7 +83,7 @@ class ProductController extends AbstractController
      */
     public function delete(Request $request, Product $product): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($product);
             $entityManager->flush();
@@ -91,46 +91,4 @@ class ProductController extends AbstractController
 
         return $this->redirectToRoute('product_index');
     }
-    /**
-     * @Route("/add/{id}", name="cart_add")
-     */
-    public function addAction($id)
-    {
-        // fetch the cart
-        $em = $this->getDoctrine()->getManager();
-        $product = $em->getRepository('WebshopBundle:Producten')->find($id);
-        //print_r($product->getId()); die;
-        $session = $this->get('request_stack')->getCurrentRequest()->getSession();
-        $cart = $session->get('cart', array());
-
-        // check if the $id already exists in it.
-        if ($product == NULL) {
-            $this->get('session')->setFlash('notice', 'This product is not     available in Stores');
-            return $this->redirect($this->generateUrl('cart'));
-        } else {
-            if (isset($cart[$id])) {
-
-                ++$cart[$id];
-                /*  $qtyAvailable = $product->getQuantity();
-                  if ($qtyAvailable >= $cart[$id]['quantity'] + 1) {
-                      $cart[$id]['quantity'] = $cart[$id]['quantity'] + 1;
-                  } else {
-                      $this->get('session')->setFlash('notice', 'Quantity     exceeds the available stock');
-                      return $this->redirect($this->generateUrl('cart'));
-                  } */
-            } else {
-                // if it doesnt make it 1
-                $cart[$id] = 1;
-                //$cart[$id]['quantity'] = 1;
-            }
-
-            $session->set('cart', $cart);
-            //echo('<pre>');
-            //print_r($cart); echo ('</pre>');die();
-            return $this->redirect($this->generateUrl('cart'));
-
-        }
-    }
 }
-
-
